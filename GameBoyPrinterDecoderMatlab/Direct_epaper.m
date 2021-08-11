7c5ba975a656e8297caf392168082f0c210299ad
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2021-08-11 16:07:08 +0200

    Add files via upload

--- GameBoyPrinterDecoderMatlab/Direct_epaper.m
+++ GameBoyPrinterDecoderMatlab/Direct_epaper.m
@@ -1,14 +1,21 @@
 % Raphaël BOICHOT 11/08/2021 Game Boy printer emulator
 % direct epaper encoder from image
-%image must be [0 84 168 255], which is the native output format in this
+%image must be 4 colors maximum, which is the native output format in this
 %project
 %Must use black and white image as input
 clear
 clc
 
 DateString = date;
-BandW_image=imread('GameBoy pixel perfect 1 10-Aug-2021.png');
-[epaper]=epaper_packet(BandW_image);
+BandW_image=imread('BBY_0001.png');
+map=BandW_image(:,:,1);
+C=unique(map);
+switch length(C)
+    case 4; map=(map==C(1))*0+(map==C(2))*84+(map==C(3))*168+(map==C(4))*255;
+    case 3; map=(map==C(1))*0+(map==C(2))*84+(map==C(3))*255;    
+    case 2; map=(map==C(1))*0+(map==C(2))*255;  
+end;
+[epaper]=epaper_packet(map);
 epaper=imresize(epaper,0.3,'bilinear'); % this improves visual aspect
-imwrite(epaper,['GameBoy pixel perfect ',DateString,'.png'])
+imwrite(epaper,['GameBoy epaper ',DateString,'.png'])
 disp('Done !')
