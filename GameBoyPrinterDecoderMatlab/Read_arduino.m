558bea1f5f707449b14f503ee7466e536899aa53
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2021-08-12 22:42:46 +0200

    Delete Read_arduino.m

--- GameBoyPrinterDecoderMatlab/Read_arduino.m
+++
@@ -1,8 +0,0 @@
-arduinoObj = serialport("COM4",115200)
-configureTerminator(arduinoObj,"CR/LF");
-flush(arduinoObj);
-arduinoObj.UserData = struct("Data",[],"Count",1)
-
-while true
-data = readline(arduinoObj)
-end
