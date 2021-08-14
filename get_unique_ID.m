555ed815441aa60db5c10a5b3c239151a38e7dbc
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2021-08-14 09:52:13 +0200

    Delete get_unique_ID.m

--- get_unique_ID.m
+++
@@ -1,5 +0,0 @@
-function [ID]=get_unique_ID(number_digits)
-library='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-for i=1:1:number_digits
-    ID(i)=library(ceil(rand*62));
-end
