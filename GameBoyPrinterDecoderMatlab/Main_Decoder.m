a23b90bb5866b728e8f78b1f30eb90efe48e95d9
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2021-08-18 22:00:56 +0200

    Add files via upload

--- GameBoyPrinterDecoderMatlab/Main_Decoder.m
+++ GameBoyPrinterDecoderMatlab/Main_Decoder.m
@@ -7,7 +7,7 @@ clc
 % Here you enter some parameters
 %------------------------------------------------------------------------
 paper_color=1;%6=random, 5=purple, 4=pink, 3=regular blue, 2=regular yellow or 1=regular white
-%watermarking='RaphaÃ«l BOICHOT 2021';
+%watermarking='Raphaël BOICHOT 2021';
 file='Entry_file.txt';% enter text file to decode
 color_option=1; %1 for Black and white, 2 for Game Boy Color, 3 for Game Boy DMG, 4 for CGA, for pixel perfect output
 Timeout_printing=0;  %0 to separate images automatically if margin >0
